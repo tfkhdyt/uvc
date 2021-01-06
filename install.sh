5cea1455972cbda40d7a88a45ed155f2f02a5465
tfkhdyt <47195537+tfkhdyt@users.noreply.github.com>
2021-01-07 05:06:28 +0700

    Fix installer

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 clear
 pkg install ffmpeg
 # alias='cd ~/x265 && sh x265.sh'
 # alias x265=$alia
 echo "alias x265='cd ~/x265 && sh x265.sh'" >> ~/.zshrc
 echo "alias x265='cd ~/x265 && sh x265.sh'" >> ~/../usr/etc/bash.bashrc
+$SHELL
 echo "Instalasi sukses!"
+echo "Ketik x265 untuk memulai program"
 
 
