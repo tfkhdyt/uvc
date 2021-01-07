c2af55b4fe784ddf3b5bd52c3914284cb1759b21
Taufik Hidayat <47195537+tfkhdyt@users.noreply.github.com>
2021-01-07 21:31:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-pkg install ffmpeg toilet ruby figlet -y
+pkg install ffmpeg figlet ruby toilet
 gem install lolcat
 echo "alias x265='cd ~/x265 && sh x265.sh'" >> ~/.zshrc
 echo "alias x265='cd ~/x265 && sh x265.sh'" >> ~/../usr/etc/bash.bashrc
