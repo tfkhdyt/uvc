dda60c9a27767300ea6c51dae4ff260ecd0a3506
tfkhdyt <ta142fa@gmail.com>
2021-01-09 10:39:47 +0700

    Mengubah metode update

--- update.sh
+++ update.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-cd ~
-sed -i '/uvc/d' .zshrc
-sed -i '/uvc/d' ../usr/etc/bash.bashrc
-rm -rf uvc/
-cd ~
-git clone https://github.com/tfkhdyt/uvc.git
 cd ~/uvc
-./install.sh
+git pull
