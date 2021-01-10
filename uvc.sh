bb598ba990c27b99a8d9a289e3636578d0635d0a
tfkhdyt <ta142fa@gmail.com>
2021-01-10 11:32:12 +0700

    uvc.sh -> Memperbaiki indentasi

--- uvc.sh
+++ uvc.sh
@@ -1,150 +1,150 @@
 #!/bin/bash
 # developed by tfkhdyt
 logo(){
-	toilet -f smslant -F border -w 48 "Ultra Video Converter" | lolcat
+  toilet -f smslant -F border -w 48 "Ultra Video Converter" | lolcat
 }
 proses(){
-clear
-case $pilihan in
-  1)
-  cd ../storage/shared
-  while [ -z "$namaFileInput" ]; do
-   browse(){
-   toilet -f smslant -F border "Pilih video" | lolcat
-   pwd -P
-   tree -L 1
-   echo "\n    [1] = Buka folder"
-   echo "    [2] = Pilih file"
-   echo "    [3] = Kembali ke menu utama\n"
-   printf "  Pilih: "
-   read exp
-   if [ $exp -eq 1 ]; then
-      echo "\n  * Tanpa tanda petik"
-      echo "  .. untuk keluar folder"
-      printf "\n  Nama folder: "
-      read namaFolder
-      cd "$namaFolder"
-      clear
-    elif [ $exp -eq 2 ]; then
-      echo "\n  * Tanpa tanda petik"
-      printf "  Nama file          : "
-      read namaFileInput
-      printf "  Simpan dengan nama : "
-      read namaFileOutput
-      clear
-      konfirmasi(){
-      toilet -f smslant -F border "Konfirmasi" | lolcat
-      ptInput="$namaFileInput"
-      ptOutput="$namaFileOutput"
-      durasi=$(ffprobe -i "$namaFileInput" -show_entries format=duration -v quiet -of csv='p=0' -sexagesimal | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')
-      echo "  Input  : $ptInput"
-      echo "  Output : $ptOutput"
-      echo "  Durasi : $durasi\n"
-      echo "  Apakah anda ingin memotong/trim videonya?"
-      echo "    [1] Iya, trim video terlebih dahulu"
-      echo "    [2] Tidak, langsung convert saja"
-      echo "    [3] Batal"
-      printf "\n  Pilih : "
-      read exp2
-      if [ $exp2 -eq 2 ]; then
-        clear
-        echo "Proses convert akan segera dimulai..."
-	echo "Tekan ctrl + c untuk membatalkan proses convert"
-	sleep 3
-        ffmpeg -i "$namaFileInput" -c:v libx265 -crf 25 -preset ultrafast -c:a copy "$namaFileOutput"
-cd ~
-    unset pilihan
-    unset exp
-    unset exp2
-    unset exp3
-    unset waktuMulai
-        unset waktuSelesai
-    unset namaFileInput
-    unset namaFileOutput
-    unset namaFolder
-    clear
-    echo "Proses convert telah selesai"
-    exit
-      elif [ $exp2 -eq 3 ]; then
-        clear
-        unset exp
-        unset exp2
-        unset namaFileInput
-        unset namaFileOutput
-        unset namaFolder
-        browse
-      elif [ $exp2 -eq 1 ]; then
-        clear
-        trim(){
-        toilet -f smslant -F border "Trim" | lolcat
-        ptInput="$namaFileInput"
-        ptOutput="$namaFileOutput"
-      # durasi=$(ffprobe -i "$namaFileInput" -show_entries format=duration -v quiet -of csv='p=0' -sexagesimal | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')
-        echo "  Input  : $ptInput"
-        echo "  Output : $ptOutput"
-        echo "  Durasi : $durasi\n"
-        echo "  Isi dengan format 'jam:menit:detik'"
-        echo "  Contoh : 00:01:25\n"
-        printf "  Waktu mulai video   : "
-        read waktuMulai
-        printf "  Waktu selesai video : "
-        read waktuSelesai
-        printf "\n  Mulai convert? (y/n) : "
-        read exp3
-        if [ "$exp3" = "y" ]; then
+  clear
+  case $pilihan in
+    1)
+      cd ../storage/shared
+      while [ -z "$namaFileInput" ]; do
+        browse(){
+          toilet -f smslant -F border "Pilih video" | lolcat
+          pwd -P
+          tree -L 1
+          echo "\n    [1] = Buka folder"
+          echo "    [2] = Pilih file"
+          echo "    [3] = Kembali ke menu utama\n"
+          printf "  Pilih: "
+          read exp
+          if [ $exp -eq 1 ]; then
+            echo "\n  * Tanpa tanda petik"
+            echo "  .. untuk keluar folder"
+            printf "\n  Nama folder: "
+            read namaFolder
+            cd "$namaFolder"
+            clear
+          elif [ $exp -eq 2 ]; then
+            echo "\n  * Tanpa tanda petik"
+            printf "  Nama file          : "
+            read namaFileInput
+            printf "  Simpan dengan nama : "
+            read namaFileOutput
+            clear
+            konfirmasi(){
+              toilet -f smslant -F border "Konfirmasi" | lolcat
+              ptInput="$namaFileInput"
+              ptOutput="$namaFileOutput"
+              durasi=$(ffprobe -i "$namaFileInput" -show_entries format=duration -v quiet -of csv='p=0' -sexagesimal | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')
+              echo "  Input  : $ptInput"
+              echo "  Output : $ptOutput"
+              echo "  Durasi : $durasi\n"
+              echo "  Apakah anda ingin memotong/trim videonya?"
+              echo "    [1] Iya, trim video terlebih dahulu"
+              echo "    [2] Tidak, langsung convert saja"
+              echo "    [3] Batal"
+              printf "\n  Pilih : "
+              read exp2
+              if [ $exp2 -eq 2 ]; then
+                clear
+                echo "Proses convert akan segera dimulai..."
+                echo "Tekan ctrl + c untuk membatalkan proses convert"
+                sleep 3
+                ffmpeg -i "$namaFileInput" -c:v libx265 -crf 25 -preset ultrafast -c:a copy "$namaFileOutput"
+                cd ~
+                unset pilihan
+                unset exp
+                unset exp2
+                unset exp3
+                unset waktuMulai
+                unset waktuSelesai
+                unset namaFileInput
+                unset namaFileOutput
+                unset namaFolder
+                clear
+                echo "Proses convert telah selesai"
+                exit
+              elif [ $exp2 -eq 3 ]; then
+                clear
+                unset exp
+                unset exp2
+                unset namaFileInput
+                unset namaFileOutput
+                unset namaFolder
+                browse
+              elif [ $exp2 -eq 1 ]; then
+                clear
+                trim(){
+                  toilet -f smslant -F border "Trim" | lolcat
+                  ptInput="$namaFileInput"
+                  ptOutput="$namaFileOutput"
+                  # durasi=$(ffprobe -i "$namaFileInput" -show_entries format=duration -v quiet -of csv='p=0' -sexagesimal | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')
+                  echo "  Input  : $ptInput"
+                  echo "  Output : $ptOutput"
+                  echo "  Durasi : $durasi\n"
+                  echo "  Isi dengan format 'jam:menit:detik'"
+                  echo "  Contoh : 00:01:25\n"
+                  printf "  Waktu mulai video   : "
+                  read waktuMulai
+                  printf "  Waktu selesai video : "
+                  read waktuSelesai
+                  printf "\n  Mulai convert? (y/n) : "
+                  read exp3
+                  if [ "$exp3" = "y" ]; then
+                    clear
+                    echo "Proses convert akan segera dimulai..."
+                    echo "Tekan ctrl + c untuk membatalkan proses convert"
+                    sleep 3
+                    ffmpeg -i "$namaFileInput" -ss "$waktuMulai" -to "$waktuSelesai" -c:v libx265 -crf 25 -preset ultrafast -c:a copy "$namaFileOutput"
+                    cd ~
+                    unset pilihan
+                    unset exp
+                    unset exp2
+                    unset exp3
+                    unset waktuMulai
+                    unset waktuSelesai
+                    unset namaFileInput
+                    unset namaFileOutput
+                    unset namaFolder
+                    clear
+                    echo "Proses convert telah selesai"
+                    exit 
+                  elif [ "$exp3" = "n" ]; then
+                    clear
+                    konfirmasi
+                    echo "Input tidak valid"
+                    trim
+                  fi
+                }
+              trim
+            else
+              clear
+              echo "Input tidak valid"
+              konfirmasi
+              fi
+            }
+          konfirmasi
+        elif [ $exp -eq 3 ]; then
+          cd ~/uvc
           clear
-          echo "Proses convert akan segera dimulai..."
-          echo "Tekan ctrl + c untuk membatalkan proses convert"
-          sleep 3
-          ffmpeg -i "$namaFileInput" -ss "$waktuMulai" -to "$waktuSelesai" -c:v libx265 -crf 25 -preset ultrafast -c:a copy "$namaFileOutput"
-          cd ~
-    unset pilihan
-    unset exp
-    unset exp2
-    unset exp3
-    unset waktuMulai
-        unset waktuSelesai
-    unset namaFileInput
-    unset namaFileOutput
-    unset namaFolder
-    clear
-    echo "Proses convert telah selesai"
-    exit 
-      elif [ "$exp3" = "n" ]; then
-         clear
-         konfirmasi
-         echo "Input tidak valid"
-        trim
-       fi
-       }
-       trim
-       else
-       clear
-         echo "Input tidak valid"
-       konfirmasi
-      fi
-      }
-      konfirmasi
-     elif [ $exp -eq 3 ]; then
-       cd ~/uvc
-       clear
-       unset pilihan
-        unset exp
-        unset exp2
-        unset exp3
-	unset waktuMulai
-        unset waktuSelesai
-        unset namaFileInput
-        unset namaFileOutput
-        unset namaFolder
-       menu
-      else
-        clear
-        echo "Input tidak valid"
-    fi
-  }
-  browse
-  done
+          unset pilihan
+          unset exp
+          unset exp2
+          unset exp3
+          unset waktuMulai
+          unset waktuSelesai
+          unset namaFileInput
+          unset namaFileOutput
+          unset namaFolder
+          menu
+        else
+          clear
+          echo "Input tidak valid"
+          fi
+        }
+      browse
+    done
     ;;
   2)
     clear
@@ -154,7 +154,7 @@ cd ~
     unset exp2
     unset exp3
     unset waktuMulai
-        unset waktuSelesai
+    unset waktuSelesai
     unset namaFileInput
     unset namaFileOutput
     unset namaFolder
@@ -170,13 +170,13 @@ cd ~
 esac
 }
 menu(){
-logo
-printf "  Menu: \n"
-printf "    [1] = Pilih video\n"
-printf "    [2] = Keluar\n\n"
-printf "  Pilih: "
-read pilihan
-proses
+  logo
+  printf "  Menu: \n"
+  printf "    [1] = Pilih video\n"
+  printf "    [2] = Keluar\n\n"
+  printf "  Pilih: "
+  read pilihan
+  proses
 }
 clear
 menu
