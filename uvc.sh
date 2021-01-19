ece413cdb5fb515de0afbfb0c9471e6b7179d676
tfkhdyt <ta142fa@gmail.com>
2021-01-19 09:15:19 +0700

    uvc.sh -> menambah fitur self update

--- uvc.sh
+++ uvc.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 # developed by tfkhdyt
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTNAME="$0"
+ARGS="$@"
+BRANCH="main"
+
+self_update() {
+  cd $SCRIPTPATH
+  git fetch
+
+  [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+    echo "Found a new version of me, updating myself..."
+      git pull --force
+      git checkout $BRANCH
+      git pull --force
+      echo "Running the new version..."
+      exec "$SCRIPTNAME" "$@"
+
+        # Now exit this old instance
+        exit 1
+      }
+    echo "Already the latest version."
+  }
+
+self_update
 color(){
   R='\033[0;31m'
   NC='\033[0m' # No Color
